1b3d6fb0962a10ad098f29ffa7a85101ccc09719
lmptc <lmptcy@gmail.com>
2024-11-05 17:00:40 -0500

    first commit

---
+++ plot_utilities.py
@@ -0,0 +1,17 @@
+# Colorbar 
+
+#      ax = plt.subplot(nrows,ncols,it+1) 
+
+#         im = ax.imshow(images[it,:,:],cmap="gray",vmin = vmin, vmax=vmax) 
+
+#         divider = make_axes_locatable(ax) 
+
+#         cax = divider.append_axes('right', size='5%', pad=0.05) 
+
+#         fig.colorbar(im, cax=cax, orientation='vertical') 
+
+ 
+
+# Plot nrow and ncol 
+
+# Plot  
